1096d2758d175d8864681baa579f6f28fa0bb6c6
Cris <cristiana.grigoriu@gmail.com>
2020-07-13 22:24:22 +0300

    Generate more quiz questions in quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,14 @@
 import React from "react";
+import QuizQuestion from "./QuizQuestion";
 
 export default function Quiz() {
-  return <div>Quiz</div>;
+  function createQuestions() {
+    const questions = [];
+    for (let i = 0; i < 3; i++) {
+      questions.push(<QuizQuestion key={i} operations={("+", "-")} />);
+    }
+    return questions;
+  }
+
+  return <div>{createQuestions()}</div>;
 }
